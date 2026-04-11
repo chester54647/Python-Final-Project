d9ffe8047170e96e0768ae76ccad6166f21437d7
Chase <chaseoliver155@gmail.com>
2026-04-11 19:54:32 -0230

    Added the 'add' and 'remove' sections to the page and made base template

--- app.py
+++ app.py
@@ -10,5 +10,13 @@ def home():
 def games():
     return render_template("games.html")
 
+@app.route("/add")
+def add_game():
+    return render_template("add.html")
+
+@app.route("/remove")
+def remove_game():
+    return render_template("remove.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
