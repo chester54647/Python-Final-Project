7c966707601d399a683c7af3fb3973501a4fa6fb
Chase <chaseoliver155@gmail.com>
2026-04-16 18:27:49 -0230

    Read form values then displayed the added games to the games page

--- app.py
+++ app.py
@@ -2,18 +2,31 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+games_list = []
+
 @app.route("/")
 def home():
     return render_template("index.html")
 
 @app.route("/games")
 def games():
-    return render_template("games.html")
+    return render_template("games.html", games=games_list)
 
 @app.route("/add", methods=["GET", "POST"])
 def add_game():
     if request.method == "POST":
-        return "Form submitted"
+        title = request.form.get("title")
+        genre = request.form.get("genre")
+        platform = request.form.get("platform")
+        rating = request.form.get("rating")
+        game = {
+            "title": title,
+            "genre": genre,
+            "platform": platform,
+            "rating": rating
+        }
+        games_list.append(game)
+
     return render_template("add.html")
 
 @app.route("/remove")
